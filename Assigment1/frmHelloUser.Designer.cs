c086aae8588ef03255dbec6bfb0fdce16295d3fd
MCodilan <milocee2k@gmail.com>
2016-09-26 10:37:49 -0500

    Complete 1st Question
    Finished Product

---
+++ Assigment1/frmHelloUser.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Assigment1
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPress = new System.Windows.Forms.Button();
+            this.lblGreet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPress
+            // 
+            this.btnPress.Location = new System.Drawing.Point(37, 29);
+            this.btnPress.Name = "btnPress";
+            this.btnPress.Size = new System.Drawing.Size(119, 23);
+            this.btnPress.TabIndex = 0;
+            this.btnPress.Text = "Do NOT press.";
+            this.btnPress.UseVisualStyleBackColor = true;
+            this.btnPress.Click += new System.EventHandler(this.btnPress_Click);
+            // 
+            // lblGreet
+            // 
+            this.lblGreet.AutoSize = true;
+            this.lblGreet.Location = new System.Drawing.Point(37, 59);
+            this.lblGreet.Name = "lblGreet";
+            this.lblGreet.Size = new System.Drawing.Size(0, 13);
+            this.lblGreet.TabIndex = 1;
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblGreet);
+            this.Controls.Add(this.btnPress);
+            this.Name = "frmHelloWorld";
+            this.Text = "Hello User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPress;
+        private System.Windows.Forms.Label lblGreet;
+    }
+}
+
---
+++ Assigment1/frmHelloUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigment1
+{
+    public partial class frmHelloWorld : Form
+    {
+        public frmHelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPress_Click(object sender, EventArgs e)
+        {
+            lblGreet.Text = "Yo yo, itz yah boi MiloCee-izzle!";
+        }
+    }
+}
